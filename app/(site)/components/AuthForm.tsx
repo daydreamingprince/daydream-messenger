4674a9a5d5a7d2a51567ef522e12ea01984e3414
daydreamingprince <thedaydreamingprince@gmail.com>
2023-10-04 02:28:53 +1100

    Declarte setVariant for AuthForm

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,13 +1,23 @@
 'use client';
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
   const [variant, setVariant] = useState<Variant>('LOGIN');
   const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === 'LOGIN') {
+      setVariant('REGISTER');
+    } else {
+      setVariant('LOGIN')
+    }
+  }, [variant])
+
   
+
   return (
     <div>Daydream Auth Form</div>
    );
