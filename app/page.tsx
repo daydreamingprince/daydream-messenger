ce931ef0a84da47ca586abad468aa2d4860b9e03
daydreamingprince <thedaydreamingprince@gmail.com>
2023-10-03 21:07:39 +1100

    Improve code readability

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
     <div>
-      <p className="text-sky-500 text-3xl">Hello Daydream!</p>
+      <p
+        className="
+        text-sky-500
+          text-3xl
+        "
+      >
+        Hello Daydream!
+      </p>
     </div>
   )
 }
