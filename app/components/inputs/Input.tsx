cd573b8d7a6c27459a0796ccf80c737f6d753b4f
daydreamingprince <thedaydreamingprince@gmail.com>
2023-10-04 05:04:00 +1100

    Import required dependencies for Input.tsx

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,5 +1,22 @@
 'use client';
 
+import clsx from "clsx";
+import {
+  FieldErrors,
+  FieldValues,
+  UseFormRegister
+} from 'react-hook-form';
+
+interface InputProps {
+  label: string;
+  id: string;
+  type?: string;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>,
+  errors: FieldErrors,
+  disabled?: boolean,
+}
+
 const Input = () => {
   return ( 
     <div>
