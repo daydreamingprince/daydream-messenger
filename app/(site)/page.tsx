daf60d5e2e9b5be2936d3dee34b077c2eb0d41ee
daydreamingprince <thedaydreamingprince@gmail.com>
2023-10-03 21:53:20 +1100

    Modify logo size and add Sign in text

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -24,6 +24,19 @@ export default function Home() {
           className="mx-auto w-auto"
           src="/images/3.png"
         />
+        <h2
+          /** Sign in to your account text */
+          className="
+            mt-6
+            text-center
+            text-3xl
+            font-bold
+            tracking-tight
+            text-gray-900
+          "
+        >
+          Sign in to your account
+        </h2>
       </div>
     </div>
   )
