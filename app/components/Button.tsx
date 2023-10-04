c04c1c927b8019e0b6abbe221c160079e28758c3
daydreamingprince <thedaydreamingprince@gmail.com>
2023-10-04 15:40:02 +1100

    Add properties for Button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,3 +1,17 @@
+'use client';
+
+import clsx from "clsx";
+
+interface ButtonProps {
+  type?: 'button' | 'submit' | 'reset' | undefined;
+  fullWidth?: boolean;
+  children?: React.ReactNode;
+  onClick?: () => void;
+  secondary?: boolean;
+  danger?: boolean;
+  disabled?: boolean;
+}
+
 const Button = () => {
   return ( 
     <div>Daydream Button</div>
