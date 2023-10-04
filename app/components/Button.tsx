38a1dcecf716755ad986d2c016e7d680cd6a1524
daydreamingprince <thedaydreamingprince@gmail.com>
2023-10-04 16:20:50 +1100

    Change color of button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -12,9 +12,42 @@ interface ButtonProps {
   disabled?: boolean;
 }
 
-const Button = () => {
+const Button: React.FC<ButtonProps> = ({
+  type,
+  fullWidth,
+  children,
+  onClick,
+  secondary,
+  danger,
+  disabled
+}) => {
   return ( 
-    <div>Daydream Button</div>
+    <button
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+      className={clsx(`
+        flex
+        justify-center
+        rounded-md
+        px-3
+        py-2
+        text-sm
+        font-semibold
+        focus-visible:outline
+        focus-visible:outline-2
+        focus-visible:outline-offset-2
+      `,
+      disabled && "opacity-50 cursor-default",
+      fullWidth && "w-full",
+      secondary ? 'text-gray-900' : 'text-white',
+      danger && "bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600",
+      /** Styling for the button */
+      !secondary && !danger && "bg-orange-500 hover:bg-orange-600 focus-visible:outline-orange-600"
+      )}
+    >
+      {children}
+    </button>
    );
 }
  
