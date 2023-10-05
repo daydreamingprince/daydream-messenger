832cc8525b34a9c6539d8816a44dde1fd9ebb659
daydreamingprince <thedaydreamingprince@gmail.com>
2023-10-05 17:03:40 +1100

    Define properties for the DesktopItem

--- app/components/sidebar/DesktopItem.tsx
+++ app/components/sidebar/DesktopItem.tsx
@@ -1,4 +1,23 @@
-const DesktopItem = () => {
+'use client';
+
+import clsx from "clsx";
+import Link from "next/link";
+
+interface DesktopItemProps {
+  label: string;
+  icon: any;
+  href: string;
+  onClick?: () => void;
+  active?: boolean;
+}
+
+const DesktopItem: React.FC<DesktopItemProps> = ({
+  label,
+  icon,
+  href,
+  onClick,
+  active
+}) => {
   return ( 
     <div>
       Desktop Item
