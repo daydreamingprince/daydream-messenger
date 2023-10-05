74f0bb5a492b9202964f6ec94ff595c4e99fa9e7
daydreamingprince <thedaydreamingprince@gmail.com>
2023-10-05 16:00:44 +1100

    Add Logout button(not yet functional)

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,6 +1,12 @@
+'use client'
+
+import { signOut } from "next-auth/react";
+
 const Users = () => {
   return ( 
-    <div>Daydreamers!</div>
+    <button onClick={() => signOut()}>
+      Logout
+    </button>
    );
 }
  
