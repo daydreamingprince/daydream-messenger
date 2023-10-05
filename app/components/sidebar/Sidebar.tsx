5f33ab5ddf3423a86b53d2bd13956f6a8cde9a7f
daydreamingprince <thedaydreamingprince@gmail.com>
2023-10-05 18:33:58 +1100

    Import next-superjson-plugin

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -6,9 +6,10 @@ async function Sidebar({ children }: {
   children: React.ReactNode;
 }) {
   const currentUser = await getCurrentUser();
+
   return (
     <div className="h-full">
-      <DesktopSidebar />
+      <DesktopSidebar currentUser={currentUser} />
       <MobileFooter />
       <main className="lg:pl-20 h-full">
         {children}
