18fe462849d6e1eec745825392c954cefbb42ddb
daydreamingprince <thedaydreamingprince@gmail.com>
2023-10-05 16:54:02 +1100

    Add styling for the sidebar

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,9 +1,31 @@
 'use client';
 
+import useRoutes from "@/app/hooks/useRoutes";
+import { useState } from "react";
+
 const DesktopSidebar = () => {
+  const routes = useRoutes();
+  const [isOpen, setIsOpen] = useState(false);
 
   return ( 
-    <div>
+    <div
+      className="
+        hidden
+        lg:fixed
+        lg:inset-y-0
+        lg:left-0
+        lg:z-40
+        lg:w-20
+        xl:px-6
+        lg:overflow-y-auto
+        lg:bg-white
+        lg:border-r-[1px]
+        lg:pb-4
+        lg:flex
+        lg:flex-col
+        justify-between
+      "
+    >
       Desktop Sidebar
     </div>
    );
