fa341941e427279a2a2579195c876fad0a23b3ce
daydreamingprince <thedaydreamingprince@gmail.com>
2023-10-05 16:43:49 +1100

    Import required dependencies on useRoutes

---
+++ app/hooks/useRoutes.ts
@@ -0,0 +1,14 @@
+import { useMemo } from "react";
+import { usePathname } from "next/navigation";
+import { HiChat } from "react-icons/hi";
+import {
+  HiArrowLeftOnRectangle,
+  HiUsers
+} from "react-icons/hi2";
+import { signOut } from "next-auth/react";
+
+import useConversation from "./useConversation";
+
+const useRoutes = () => {
+  const pathname = usePathname();
+}
