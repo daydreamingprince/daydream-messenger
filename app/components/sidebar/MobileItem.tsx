c61d8c76f97f3c45dc49c0ac6308b0df99692cc4
daydreamingprince <thedaydreamingprince@gmail.com>
2023-10-05 17:44:11 +1100

    Add icons for mobile footer

--- app/components/sidebar/MobileItem.tsx
+++ app/components/sidebar/MobileItem.tsx
@@ -1,8 +1,31 @@
-const MobileItem = () => {
+"use client";
+
+import Link from "next/link";
+import clsx from "clsx";
+
+interface MobileItemProps {
+  href: string;
+  icon: any;
+  active?: boolean;
+  onClick?: () => void;
+}
+
+const MobileItem: React.FC<MobileItemProps> = ({
+  href,
+  icon: Icon,
+  active,
+  onClick
+}) => {
+  const handleClick = () => {
+    if (onClick) {
+      return onClick();
+    }
+  }
+
   return ( 
-    <div>
-      Mobile Item
-    </div>
+    <Link href={href}>
+      <Icon />
+    </Link>
    );
 }
  
