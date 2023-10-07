e4875229badf2a3e2716cfef1a8a42a64b9a061f
daydreamingprince <thedaydreamingprince@gmail.com>
2023-10-07 20:03:48 +1100

    Update getConversations.ts

--- app/actions/getConversations.ts
+++ app/actions/getConversations.ts
@@ -1,6 +1,5 @@
 import prisma from "@/app/libs/prismadb";
 import getCurrentUser from "./getCurrentUser";
-import getCurrentUser from "./getCurrentUser";
 
 const getConversations = async () => {
   const currentUser = await getCurrentUser();
@@ -10,8 +9,30 @@ const getConversations = async () => {
   }
 
   try {
+    const conversations = await prisma.conversation.findMany({
+      orderBy: {
+        lastMessageAt: 'desc'
+      },
+      where: {
+        userIds: {
+          has: currentUser.id
+        }
+      },
+      include: {
+        users: true,
+        messages: {
+          include: {
+            sender: true,
+            seen: true
+          }
+        }
+      }
+    });
 
+    return conversations;
   } catch (error: any) {
     return [];
   }
-}
+};
+
+export default getConversations;
