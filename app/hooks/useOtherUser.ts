bf662c79c8708ddde93cba4bd061c10aeca51e4f
daydreamingprince <thedaydreamingprince@gmail.com>
2023-10-08 15:43:12 +1100

    Add useOtherUser.ts

---
+++ app/hooks/useOtherUser.ts
@@ -0,0 +1,12 @@
+import { useSession } from "next-auth/react";
+import { useMemo } from "react";
+import { FullConversationType } from "../types";
+import { User } from "@prisma/client";
+
+const useOtherUser = (conversation: FullConversationType | {
+  users: User[]
+}) => {
+
+};
+
+export default useOtherUser;
