bae43ba0524e3231bf1b9f8e69dee4e3c30c12fe
daydreamingprince <thedaydreamingprince@gmail.com>
2023-10-09 19:44:08 +1100

    Update getMessages.ts

--- app/actions/getMessages.ts
+++ app/actions/getMessages.ts
@@ -4,8 +4,23 @@ const getMessages = async (
   conversationId: string
 ) => {
   try {
+    const messages = await prisma.message.findMany({
+      where: {
+        conversationId: conversationId
+      },
+      include: {
+        sender: true,
+        seen: true,
+      },
+      orderBy: {
+        createdAt: 'asc'
+      }
+    });
 
+    return messages;
   } catch (error: any) {
     return [];
   }
-}
+};
+
+export default getMessages;
